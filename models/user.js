d3677f1e1e8b882b9c7754c60b70446030d240c9
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2023-09-16 20:33:16 +0300

    changes schemas

--- models/user.js
+++ models/user.js
@@ -1,39 +1,32 @@
-const { Schema, model } = require('mongoose');
-const { handleMongooseError } = require('../helpers');
-const Joi = require('joi');
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../helpers");
+const Joi = require("joi");
 
-// eslint-disable-next-line no-useless-escape
-const emailRegex =  /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-const passwordRegex = /^(?=.*[a-zA-Z]{6})(?=.*\d)[a-zA-Z\d]{7}$/;
+const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d).{6,}$/;
 const dateRegexp = /^\d{2}-\d{2}-\d{4}$/;
 const bloodList = [1, 2, 3, 4];
 const sexList = ["male", "female"];
-const levelActivityList = [1, 2, 3, 4, 5]
+const levelActivityList = [1, 2, 3, 4, 5];
 
 const userSchema = new Schema(
   {
     name: {
       type: String,
-      required: [true, 'John Doe'],
+      required: [true, "Name is required"],
     },
     email: {
       type: String,
-      matchMedia: emailRegex,
+      match: emailRegex,
       unique: true,
       required: true,
     },
     password: {
       type: String,
-      // ? перевіріті паттерн для пассв
-      matchMedia: passwordRegex,
+      match: passwordRegex,
       minlength: 6,
       required: true,
     },
-    // subscription: {
-    //   type: String,
-    //   enum: ['starter', 'pro', 'business'],
-    //   default: 'starter',
-    // },
     accessToken: {
       type: String,
       default: null,
@@ -49,61 +42,50 @@ const userSchema = new Schema(
     height: {
       type: Number,
       min: 150,
-      required:true,
+      required: true,
     },
     currentWeight: {
       type: Number,
       min: 35,
-      required:true,
+      required: true,
     },
     desiredWeight: {
       type: Number,
       min: 35,
-      required:true,
+      required: true,
     },
-
     birthday: {
-    type: Date,
-    required: true,
-    validate: {
-      validator: function (value) {
-        // Перевірка, чи вік більше або рівний 18 рокам
-        const age = (new Date() - value) / (1000 * 60 * 60 * 24 * 365); // Розрахунок віку в роках
-        return age >= 18;
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (value) {
+          const age = (new Date() - value) / (1000 * 60 * 60 * 24 * 365);
+          return age >= 18;
+        },
+        message: "You must be at least 18 years old.",
       },
-      message: 'Ви повинні бути старше 18 років.',
     },
-  },
-    // birthday: {
-    //     type: String,
-    //   match: dateRegexp,
-    //   // ! add checking - older 18 years
-    //   requered: true,
-    // },
-    
     blood: {
       type: Number,
       enum: bloodList,
-      requered: true,
+      required: true,
     },
     sex: {
       type: String,
       enum: sexList,
-      requered: true,
+      required: true,
     },
     levelActivity: {
       type: Number,
       enum: levelActivityList,
-      requered: true,
+      required: true,
     },
-
-
   },
-  { versionKey: false, timestamps: true },
+  { versionKey: false, timestamps: true }
 );
 
-userSchema.post('save', handleMongooseError);
-const User = model('user', userSchema);
+userSchema.post("save", handleMongooseError);
+const User = model("user", userSchema);
 
 const registerSchema = Joi.object({
   name: Joi.string().min(2).required(),
@@ -115,29 +97,26 @@ const loginSchema = Joi.object({
   password: Joi.string().pattern(passwordRegex).required(),
 });
 
-
 const userDataSchema = Joi.object({
   height: Joi.number().min(150).required(),
-  
-    currentWeight: Joi.number().min(30).required(),
-    desiredWeight: Joi.number().min(30).required(),
+  currentWeight: Joi.number().min(35).required(),
+  desiredWeight: Joi.number().min(35).required(),
+  birthday: Joi.date().max("18 years").iso().required(),
+  blood: Joi.number()
+    .valid(...bloodList)
+    .required(),
+  sex: Joi.string()
+    .valid(...sexList)
+    .required(),
+  levelActivity: Joi.number()
+    .valid(...levelActivityList)
+    .required(),
+});
 
-   birthday: Joi.date()
-    .max('now')
-    .iso()
-    .required()
-    .less('18 years'),
-
-  // birthday: Joi.string().pattern(dateRegexp).required(),
-    blood: Joi.number().valid(...bloodList).required(),
-    sex: Joi.string().valid(...sexList).required(),
-    levelActivity: Joi.number().valid(...levelActivityList).required(),
-  
-})
 const schemas = {
   registerSchema,
   loginSchema,
-  userDataSchema
-
+  userDataSchema,
 };
+
 module.exports = { User, schemas };
