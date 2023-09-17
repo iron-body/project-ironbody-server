e0bcc3e50c897fab2ed45c01cee9d0b720574719
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2023-09-17 15:10:53 +0200

    исправил ошибку валидации даты в models/user.js

--- models/user.js
+++ models/user.js
@@ -1,20 +1,20 @@
-const { Schema, model } = require('mongoose');
-const { handleMongooseError } = require('../helpers');
-const Joi = require('joi');
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../helpers");
+const Joi = require("joi");
 
 // eslint-disable-next-line no-useless-escape
-const emailRegex =  /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 const passwordRegex = /^(?=.*[a-zA-Z]{6})(?=.*\d)[a-zA-Z\d]{7}$/;
 const dateRegexp = /^\d{2}-\d{2}-\d{4}$/;
 const bloodList = [1, 2, 3, 4];
 const sexList = ["male", "female"];
-const levelActivityList = [1, 2, 3, 4, 5]
+const levelActivityList = [1, 2, 3, 4, 5];
 
 const userSchema = new Schema(
   {
     name: {
       type: String,
-      required: [true, 'John Doe'],
+      required: [true, "John Doe"],
     },
     email: {
       type: String,
@@ -49,38 +49,38 @@ const userSchema = new Schema(
     height: {
       type: Number,
       min: 150,
-      required:true,
+      required: true,
     },
     currentWeight: {
       type: Number,
       min: 35,
-      required:true,
+      required: true,
     },
     desiredWeight: {
       type: Number,
       min: 35,
-      required:true,
+      required: true,
+    },
+    // строку 65-76 можна видалити т.я.це буде оброблено перевіркою формату дати.
+    // birthday: {
+    // type: Date,
+    // required: true,
+    // validate: {
+    //   validator: function (value) {
+    //     // Перевірка, чи вік більше або рівний 18 рокам
+    //     const age = (new Date() - value) / (1000 * 60 * 60 * 24 * 365); // Розрахунок віку в роках
+    //     return age >= 18;
+    //   },
+    //   message: 'Ви повинні бути старше 18 років.',
+    // },
+    // },
+    birthday: {
+      type: String,
+      match: dateRegexp,
+      // ! add checking - older 18 years
+      requered: true,
     },
 
-    birthday: {
-    type: Date,
-    required: true,
-    validate: {
-      validator: function (value) {
-        // Перевірка, чи вік більше або рівний 18 рокам
-        const age = (new Date() - value) / (1000 * 60 * 60 * 24 * 365); // Розрахунок віку в роках
-        return age >= 18;
-      },
-      message: 'Ви повинні бути старше 18 років.',
-    },
-  },
-    // birthday: {
-    //     type: String,
-    //   match: dateRegexp,
-    //   // ! add checking - older 18 years
-    //   requered: true,
-    // },
-    
     blood: {
       type: Number,
       enum: bloodList,
@@ -96,14 +96,12 @@ const userSchema = new Schema(
       enum: levelActivityList,
       requered: true,
     },
-
-
   },
-  { versionKey: false, timestamps: true },
+  { versionKey: false, timestamps: true }
 );
 
-userSchema.post('save', handleMongooseError);
-const User = model('user', userSchema);
+userSchema.post("save", handleMongooseError);
+const User = model("user", userSchema);
 
 const registerSchema = Joi.object({
   name: Joi.string().min(2).required(),
@@ -115,29 +113,50 @@ const loginSchema = Joi.object({
   password: Joi.string().pattern(passwordRegex).required(),
 });
 
-
 const userDataSchema = Joi.object({
   height: Joi.number().min(150).required(),
-  
-    currentWeight: Joi.number().min(30).required(),
-    desiredWeight: Joi.number().min(30).required(),
+  currentWeight: Joi.number().min(30).required(),
+  desiredWeight: Joi.number().min(30).required(),
 
-   birthday: Joi.date()
-    .max('now')
+  // birthday: Joi.date()
+  //   .max("now")
+  //   .iso()
+  //   .required()
+  //   .less("18 years")//тут была ошибка
+  birthday: Joi.date()
+    .max(
+      new Date(
+        new Date().setFullYear(new Date().getFullYear() - 18)
+      ).toISOString()
+    )
     .iso()
     .required()
-    .less('18 years'),
+    .custom((value, helpers) => {
+      // Добавляем логирование перед валидацией
+      console.log("Дата рождения перед валидацией:", value);
 
+      // Проверка на возраст больше или равно 18 лет
+      const age = (new Date() - new Date(value)) / (1000 * 60 * 60 * 24 * 365); // Расчет возраста
+      if (age >= 18) {
+        return value; // Вернуть значение, если валидация успешна
+      } else {
+        return helpers.message("Ви повинні бути старше 18 років."); // Вернуть ошибку, если не прошла валидация
+      }
+    }),
   // birthday: Joi.string().pattern(dateRegexp).required(),
-    blood: Joi.number().valid(...bloodList).required(),
-    sex: Joi.string().valid(...sexList).required(),
-    levelActivity: Joi.number().valid(...levelActivityList).required(),
-  
-})
+  blood: Joi.number()
+    .valid(...bloodList)
+    .required(),
+  sex: Joi.string()
+    .valid(...sexList)
+    .required(),
+  levelActivity: Joi.number()
+    .valid(...levelActivityList)
+    .required(),
+});
 const schemas = {
   registerSchema,
   loginSchema,
-  userDataSchema
-
+  userDataSchema,
 };
 module.exports = { User, schemas };
