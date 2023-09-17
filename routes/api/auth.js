da31f304de8e319f075248d3cd145eafdfaa75af
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2023-09-17 17:28:46 +0200

    test commit

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,20 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { schemas } = require('../../models/user');
-const { validateBody, auth, upload } = require('../../middlewares');
-const ctrl = require('../../controllers/auth');
+const { schemas } = require("../../models/user");
+const { validateBody, auth, upload } = require("../../middlewares");
+const ctrl = require("../../controllers/auth");
 
-router.post('/register', validateBody(schemas.registerSchema), ctrl.registerCtrl);
-router.post('/login', validateBody(schemas.loginSchema), ctrl.loginCtrl);
-router.get('/current', auth, ctrl.getCurrentCtrl);
-router.post('/logout', auth, ctrl.logoutCtrl);
+router.post(
+  "/register",
+  validateBody(schemas.registerSchema),
+  ctrl.registerCtrl
+);
+router.post("/login", validateBody(schemas.loginSchema), ctrl.loginCtrl);
+router.get("/current", auth, ctrl.getCurrentCtrl);
+router.post("/logout", auth, ctrl.logoutCtrl);
 router.patch(
-  '/',
+  "/",
   auth,
   validateBody(schemas.updateSubscriptionSchema),
-  ctrl.updateSubscriptionCtrl,
+  ctrl.updateSubscriptionCtrl
 );
 router.patch('/avatars', auth, upload.single('avatar'), ctrl.updateAvatarCtrl);
 router.post('/refresh', ctrl.refreshCtrl);
+
 
 module.exports = router;
