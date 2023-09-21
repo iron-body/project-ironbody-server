4a8e6ec3811c2493f9ae50048c2d6b608268b370
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2023-09-21 15:05:59 +0300

    add token generation after register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,23 @@
-const { ctrlWrapper, HttpError } = require("../helpers");
-const { User } = require("../models/user");
+const { ctrlWrapper, HttpError } = require('../helpers');
+const { User } = require('../models/user');
 // const { UserData } = require("../models/user_data");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const gravatar = require("gravatar");
-const Jimp = require("jimp");
-const path = require("path");
-const fs = require("fs/promises");
-const { updateNameAvatarSchema } = require("../models/user");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const gravatar = require('gravatar');
+const Jimp = require('jimp');
+const path = require('path');
+const fs = require('fs/promises');
+const { updateNameAvatarSchema } = require('../models/user');
 
 const { SECRET_KEY } = process.env;
-const avatarsDir = path.join(__dirname, "../", "public", "avatars");
+const avatarsDir = path.join(__dirname, '../', 'public', 'avatars');
 
 // Функція для реєстрації нового користувача
 const registerCtrl = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
-    throw HttpError(409, "Email in use"); // Помилка 409 - Конфлікт
+    throw HttpError(409, 'Email in use'); // Помилка 409 - Конфлікт
   }
   const hashedPassword = await bcrypt.hash(password, 10);
   const avatarUrl = gravatar.url(email);
@@ -26,8 +26,18 @@ const registerCtrl = async (req, res) => {
     password: hashedPassword,
     avatarUrl,
   });
+
+  const newUserDBData = User.findOne({ email });
+  const payload = { id: newUserDBData._id };
+
+  const accessToken = jwt.sign(payload, SECRET_KEY, { expiresIn: '12h' });
+  await User.findByIdAndUpdate(newUserDBData._id, {
+    accessToken,
+  });
+
   res.status(201).json({
     user: { name: newUser.name, email: newUser.email },
+    accessToken,
   });
 };
 
@@ -36,15 +46,15 @@ const loginCtrl = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
-    throw HttpError(401, "Email or password is not valid"); // Помилка 401 - Не авторизовано
+    throw HttpError(401, 'Email or password is not valid'); // Помилка 401 - Не авторизовано
   }
   const comparePassword = await bcrypt.compare(password, user.password);
   if (!comparePassword) {
-    throw HttpError(401, "Email or password is not valid");
+    throw HttpError(401, 'Email or password is not valid');
   }
   const payload = { id: user._id };
 
-  const accessToken = jwt.sign(payload, SECRET_KEY, { expiresIn: "12h" });
+  const accessToken = jwt.sign(payload, SECRET_KEY, { expiresIn: '12h' });
   await User.findByIdAndUpdate(user._id, {
     accessToken,
   });
@@ -100,7 +110,7 @@ const loginCtrl = async (req, res) => {
 const logoutCtrl = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { accessToken: null });
-  res.json({ message: "Logout success" });
+  res.json({ message: 'Logout success' });
 };
 const getCurrentCtrl = (req, res) => {
   const { name, email } = req.user;
@@ -122,14 +132,14 @@ const updateUserCtrl = async (req, res) => {
       new: true,
     });
     if (!updatedUser) {
-      throw HttpError(404, "User not found");
+      throw HttpError(404, 'User not found');
     }
     res.status(200).json({
       name: updatedUser.name,
       avatarUrl: updatedUser.avatarUrl,
     });
   } else {
-    throw HttpError(400, "No changes provided");
+    throw HttpError(400, 'No changes provided');
   }
 };
 
@@ -141,7 +151,7 @@ const updateAvatarCtrl = async (req, res) => {
   await fs.rename(tempUpload, resultUpload);
   const image = await Jimp.read(resultUpload);
   await image.resize(250, 250).write(resultUpload);
-  const avatarUrl = path.join("avatars", filename);
+  const avatarUrl = path.join('avatars', filename);
   await User.findByIdAndUpdate(_id, { avatarUrl });
 
   console.log(avatarUrl);
