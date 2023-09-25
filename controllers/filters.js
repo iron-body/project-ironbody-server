3410abc7a1d3a3fd73b0dea3adfca1857d3a16db
Oleksandr Tymchenko <116674828+oleksandr-tymchenko@users.noreply.github.com>
2023-09-25 15:20:29 +0300

    fix filters

--- controllers/filters.js
+++ controllers/filters.js
@@ -1,6 +1,18 @@
 const { HttpError, ctrlWrapper } = require("../helpers");
 const { Filter } = require("../models/filter");
 
+const getAllFilters = async (req, res) => {
+    const { limit = 10, page = 1 } = req.query;
+    const skip = (page - 1) * limit;
+    const result = await Filter.find({skip, limit});
+    if (!result) {
+        throw HttpError(404, `The filter with value "${filter}" not found`);
+    }
+   
+        res.status(200).json(result);    
+};
+
+
 const getFilters = async (req, res) => {
     const { filter } = req.query;
     const result = await Filter.find({ filter: filter }, " -_id -filter -imgURL");
@@ -13,6 +25,7 @@ const getFilters = async (req, res) => {
 
 
 module.exports = {
-  getFilters: ctrlWrapper(getFilters),
+    getFilters: ctrlWrapper(getFilters),
+    getAllFilters: ctrlWrapper(getAllFilters)
 };
 
