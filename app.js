051fc5db1de97d7a491d3563707907dfdc688ad8
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2023-09-25 21:21:45 +0200

    save chenges

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ require('dotenv').config();
 // const contactsRouter = require('./routes/api/contacts');
 const authRouter = require('./routes/api/auth');
 
+const userProductsRouter = require('./routes/api/userProducts');
 const userDataProductsRouter = require('./routes/api/userDataProductsRoute');
 const calculateNormsRouter = require('./routes/api/calculateNorms');
 const productsRouter = require('./routes/api/productsRoute');
 const productCategoriesRoute = require('./routes/api/productCategoriesRoute');
 const exercisesRouter = require('./routes/api/exercises');
+const userExercisesRouter = require('./routes/api/userExercises');
 const userDataRouter = require('./routes/api/userData');
+const filtersRouter = require('./routes/api/filters');
+
 const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
@@ -31,9 +35,13 @@ app.use('/api/userDataProducts', userDataProductsRouter);
 // app.use('/api/contacts', contactsRouter);
 app.use('/api/calculateNorms', calculateNormsRouter);
 
+app.use('/api/filters', filtersRouter);
+
 app.use('/api/products', productsRouter);
+app.use('/api/userproducts', userProductsRouter);
 app.use('/api/categories', productCategoriesRoute);
 app.use('/api/exercises', exercisesRouter);
+app.use('/api/userExercises', userExercisesRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
